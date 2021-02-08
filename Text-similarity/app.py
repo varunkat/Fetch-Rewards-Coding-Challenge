a0006c2281a59a16ba7dba16e24d8edaa1733b78
varunkat <varun.sky56@gmail.com>
2021-02-08 10:26:02 -0700

    Final Commit.

--- Text-similarity/app.py
+++ Text-similarity/app.py
@@ -102,4 +102,4 @@ def send(sum=sum):
 
 if __name__ == ' __main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80) #running application on port 80
